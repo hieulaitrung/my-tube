4dd423aec89dccd8dfb5f76aa2c95e543ac9fb54
Hieu Lai Trung <v.hieult11@vinid.net>
2020-10-02 15:43:41 +0700

    Fix server rendering

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import path from 'path'
 
 const app = express();
+app.use(express.static(path.join(__dirname, '../build')));
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../build', 'index.html'));
