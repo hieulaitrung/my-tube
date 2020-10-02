10c72205fe8893be98246e1217aaa47c1c4b0e40
Hieu Lai Trung <v.hieult11@vinid.net>
2020-10-02 15:32:34 +0700

    Fix failed build

--- src/components/TubeFetchZone.js
+++ src/components/TubeFetchZone.js
@@ -3,7 +3,6 @@ import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import CircularProgress from '@material-ui/core/CircularProgress';
-import ytdl from 'ytdl-core'
 import api from '../apis'
 
 const useStyles = makeStyles((theme) => ({
